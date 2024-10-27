32a44331baf4729aff4b1d47cf10bfeb83bcc37c
Engulfedleader2 <engulfedleader@gmail.com>
2024-10-27 09:57:06 -0400

    Initial Commit

---
+++ GroupProject/GroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupProjectApp.swift
+//  GroupProject
+//
+//  Created by Israel on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
